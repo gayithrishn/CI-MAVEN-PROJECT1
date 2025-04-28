98e2bdcbf1399fd4bdc1c997a3246660a8237320
gayithrishn <gayithrishn@gmail.com>
2025-04-28 09:28:39 +0530

    first commit

---
+++ src/main/java/com/mvit/App.java
@@ -0,0 +1,25 @@
+package com.mvit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("progran executed successfully");
+        System.out.println("this is my first jenkins CI pipeline");
+        for(int i=1;i<=10;i++)
+        {
+            if(i==4)
+            {
+                continue;
+            }
+
+            System.out.println(i);
+        }
+        System.out.println("out of the loops executed successfully");
+    }
+}
